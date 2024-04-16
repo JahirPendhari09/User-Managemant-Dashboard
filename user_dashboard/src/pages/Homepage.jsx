9c61ce80570c931df447cb627a8719efeaf6935d
JahirPendhari <jahirpp1999@gmail.com>
2024-04-16 21:01:59 +0530

    Created Button componentto avoid repeation of same button

--- user_dashboard/src/pages/Homepage.jsx
+++ user_dashboard/src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import { SingleUserCard } from "../components/SingleUser";
 import "../App.css";
 import { Modal } from "../components/Modal";
 import { deleteUserDetails, getUsersDetails, patchUsersData, postNewUserDetails } from "../Actions/action";
+import Button from "../components/Button";
 
 export const initialStateTask = { name: '', email: '',department:'' }
 
@@ -94,7 +95,7 @@ const Homepage = () => {
             <h1 className="text-3xl font-bold mb-8  mt-5">User Management Dashboard</h1>
             <div className="w-11/12 m-auto">
                 <div className="mb-5">
-                    <button type="button" onClick={handleOpenModal} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Create new user details</button>
+                    <Button onClick={handleOpenModal} text="Create new user details"/>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  gap-10">
                     {users?.length > 0 &&
@@ -111,9 +112,9 @@ const Homepage = () => {
                 </div>
             </div>
             <div className="mt-5 mb-5">
-               <button onClick={handlePageDecre} disabled={page===1} className={`text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ${page === 1 ? 'cursor-not-allowed' : 'cursor-pointer'}`}> Prev</button>
+               <Button onClick={handlePageDecre} disabled={page===1} text="Prev" />
                <span className="font-bold mx-1 mr-4">{page}</span>
-               <button onClick={handlePageIncre} disabled={users.length <= page*6} className={`text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2  ${users.length <= page*6 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>Next</button>
+               <Button onClick={handlePageIncre} disabled={users.length <= page*6} text="Next"/>
            </div>
 
             <div>
@@ -122,8 +123,8 @@ const Homepage = () => {
                         <h2 className="text-center text-2xl text-orange-400">Add New User</h2>
                         <form class="max-w-sm mx-auto" onSubmit={submitNewTodo}>
                             <div>
-                                <label className="block mb-2  text-left text-sm font-medium text-gray-900 dark:text-black">Enter name</label>
-                                <input rows="4" name="name" defaultValue={formData.name} onChange={handleChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a firstName here..." required />
+                                <label className="block mb-2  text-left text-sm font-medium text-gray-900 dark:text-black">Enter firstName and LastName</label>
+                                <input rows="4" name="name" defaultValue={formData.name} onChange={handleChange} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a name here..." required />
                             </div>
                             <div>
                                 <label className="block mb-2 mt-2 text-left text-sm font-medium text-gray-900 dark:text-black">Enter Email</label>
